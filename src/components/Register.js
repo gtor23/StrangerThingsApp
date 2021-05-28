71b9d561b849a1e10ac7592081e9cdb085ab78e6
dhui <dhui0226@gmail.com>
2021-05-28 18:06:32 -0500

    added login check

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
+function checkUsername(username) {
+    if (username.length >= 8) {
+        //console.log('username works')
+        return true
+    } else {
+        //console.log('username too short')
+        return false
+    }
+}
 
-const Register =() =>{
+function checkPassword(password, confirmPass) {
+    if (password.length >= 8 && confirmPass.length >= 8) {
+        if (password == confirmPass) {
+            //console.log('passwords match')
+            return true
+        } else {
+            //console.log('pass does not match')
+            return false
+        }
+    } else {
+        //console.log('pass too short');
+        return false
+    }
+}
 
+function checkInfo(username, password, confirmPass) {
+    if (checkUsername(username) && checkPassword(password, confirmPass)) {
+        //console.log('account made') 
+        //LEFT OFF RIGHT HERE
+    }
+}
 
+const Register = () =>{
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')    
+    const [confirmPassword, setConfirmPassword] = useState('')    
 
 
 
@@ -15,17 +47,27 @@ const Register =() =>{
         <h1>Register </h1>
                 
         <form> 
-
             <label>Username:</label>
-            <input type ='text' id='username'></input>
-
+            <input type ='text' 
+                //id='username'
+                onChange={ (event) => { setUsername(event.target.value) } }></input>
         </form>
         <form > 
-        <label>Password:</label>
-        <input type='text' id='password'></input>
+            <label>Password:</label>
+            <input type='text' 
+            id='password'
+            onChange={ (event) => { setPassword(event.target.value) } }></input>
+        </form>
+        <form > 
+            <label>Confirm Password:</label>
+            <input type='text' 
+            id='confirmPass'
+            onChange={ (event) => { setConfirmPassword(event.target.value) } }></input>
         </form>
 
-        <button className ='loginbutton'>Log In</button>
+        <button 
+            className ='createAccount' 
+            onClick={() => checkInfo(username, password, confirmPassword)}>Create Account</button>
         {/* <Link to='/register'>Register</Link> */}
 
 
